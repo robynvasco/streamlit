ad2293bd73e207e55898238c735e72b6380b0784
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-06 15:13:06 +0200

    Update unit_circle.py

--- unit_circle.py
+++ unit_circle.py
@@ -43,15 +43,15 @@ def plot_unit_circle(alpha):
     st.markdown(f"<div style='margin-bottom: 20px;'><span style='font-size: 16px;'>cos(&alpha;):</span> <span style='font-size: 18px; font-weight: bold; color: {custom_palette[0]};'>{x:.4f}</span></div>", unsafe_allow_html=True)
 
     # Draw floating labels for sin and cos lines
-    plt.text(x/2, -0.2, 'cos', ha='center', va='bottom', fontsize=12, color=custom_palette[0])
-    plt.text(x+0.2, y/2, 'sin', ha='center', va='bottom', fontsize=12, color=custom_palette[1])
+    plt.text(x/2, -0.1, 'cos', ha='center', va='bottom', fontsize=12, color=custom_palette[0])
+    plt.text(x+0.1, y/2, 'sin', ha='center', va='bottom', fontsize=12, color=custom_palette[1])
 
     # Draw the angle arc and annotate the angle value in degrees
     angle_degrees = np.degrees(alpha)
     arc = Arc((0, 0), 1, 1, 0, 0, angle_degrees, color=custom_palette[2], linewidth=1)
     plt.gca().add_patch(arc)
     angle_text = f"{angle_degrees:.1f}°"
-    plt.text(0.5, 0.15, angle_text, ha='center', va='center', fontsize=12, fontweight='bold')
+    plt.text(0.5, 0.15, angle_text, ha='center', va='center', fontsize=8,)
 
     st.pyplot(plt)
 
