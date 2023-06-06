31b5d0ca49993e53564871fe0a71a37d59b62bfb
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-06 15:15:54 +0200

    Update unit_circle.py

--- unit_circle.py
+++ unit_circle.py
@@ -2,7 +2,6 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from fractions import Fraction
 from matplotlib.patches import Arc
 
 # Set seaborn style
@@ -20,7 +19,7 @@ def plot_unit_circle(alpha):
     plt.plot([0, x], [0, 0], '-', linewidth=3, color=custom_palette[0], label='cos')
     plt.plot([x, x], [0, y], '-', linewidth=3, color=custom_palette[1], label='sin')
     plt.plot([0, x], [0, y], '-', linewidth=3, color=custom_palette[2], label='Radius')
-    plt.plot(x, y, 'o', markersize=10, color='black', label='Point')
+    plt.plot(x, y, 'o', markersize=10, color=custom_palette[2], label='Point')
     plt.plot(np.cos(np.linspace(0, 2*np.pi, 100)), np.sin(np.linspace(0, 2*np.pi, 100)), '--', linewidth=1, color='gray', alpha=0.5, label='Unit Circle')
     plt.xlim(-1.5, 1.5)
     plt.ylim(-1.5, 1.5)
@@ -31,17 +30,6 @@ def plot_unit_circle(alpha):
     plt.legend(loc='upper right', fontsize=12)
     plt.grid(True, linestyle='--', linewidth=0.5)
 
-    # Display the values of sin(alpha) and cos(alpha) in a styled box
-    box_style = {
-        'boxstyle': 'round',
-        'facecolor': 'white',
-        'edgecolor': 'gray',
-        'linewidth': 1,
-        'pad': 0.5
-    }
-    st.markdown(f"<div style='margin-top: 20px;'><span style='font-size: 16px;'>sin(&alpha;):</span> <span style='font-size: 18px; font-weight: bold; color: {custom_palette[1]};'>{y:.4f}</span></div>", unsafe_allow_html=True)
-    st.markdown(f"<div style='margin-bottom: 20px;'><span style='font-size: 16px;'>cos(&alpha;):</span> <span style='font-size: 18px; font-weight: bold; color: {custom_palette[0]};'>{x:.4f}</span></div>", unsafe_allow_html=True)
-
     # Draw floating labels for sin and cos lines
     plt.text(x/2, -0.1, 'cos', ha='center', va='bottom', fontsize=12, color=custom_palette[0])
     plt.text(x+0.1, y/2, 'sin', ha='center', va='bottom', fontsize=12, color=custom_palette[1])
@@ -50,8 +38,8 @@ def plot_unit_circle(alpha):
     angle_degrees = np.degrees(alpha)
     arc = Arc((0, 0), 1, 1, 0, 0, angle_degrees, color=custom_palette[2], linewidth=1)
     plt.gca().add_patch(arc)
-    angle_text = f"{angle_degrees:.1f}°"
-    plt.text(0.5, 0.15, angle_text, ha='center', va='center', fontsize=8,)
+    angle_text = f"Angle: {angle_degrees:.1f}°"
+    plt.text(0.5, 0.15, angle_text, ha='center', va='center', fontsize=10, color='black')
 
     st.pyplot(plt)
 
