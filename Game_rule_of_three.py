39ae5d87c4d877c632201bbec1960d5e12db32bd
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-09 12:45:45 +0200

    Update Game_rule_of_three.py

--- Game_rule_of_three.py
+++ Game_rule_of_three.py
@@ -1,45 +1,36 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+import numpy as np
 
-def rule_of_three_calculator(a, b, c):
-    # Calculate the unknown value using the Rule of Three
-    x = (b * c) / a
-    return x
+def proportional_relationship(x, y):
+    # Calculate the proportional relationship
+    k = y[0] / x[0]
+    return k
 
-def visualize_rule_of_three(a, b, c, x):
-    # Create a bar chart to visualize the proportions
-    values = [a, b, c, x]
-    labels = ['a', 'b', 'c', 'x']
-
+def visualize_proportional_relationship(x, y):
+    # Create a scatter plot to visualize the proportional relationship
     fig, ax = plt.subplots()
-    ax.bar(labels, values)
-
-    ax.annotate(str(a), xy=('a', a), xytext=(0, 5), textcoords='offset points', ha='center')
-    ax.annotate(str(b), xy=('b', b), xytext=(0, 5), textcoords='offset points', ha='center')
-    ax.annotate(str(c), xy=('c', c), xytext=(0, 5), textcoords='offset points', ha='center')
-    ax.annotate(str(round(x, 2)), xy=('x', x), xytext=(0, 5), textcoords='offset points', ha='center')
-
-    ax.set_ylim(0, max(values) * 1.2)
-    ax.set_xlabel('Values')
-    ax.set_ylabel('Quantity')
-    ax.set_title('Rule of Three Visualization')
+    ax.scatter(x, y, color='blue')
+    
+    k = proportional_relationship(x, y)
+    ax.plot(x, k * np.array(x), color='red', linestyle='--')
+    
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_title('Proportional Relationship')
 
     st.pyplot(fig)
 
 def main():
-    st.title("Rule of Three Visualization")
+    st.title("Proportional Relationship Visualization")
 
-    st.write("The Rule of Three is a mathematical principle that allows you to solve problems based on proportions. Given three known values, you can calculate the unknown value using the formula: (a / b = c / x)")
+    st.write("The proportional relationship is a key concept in the Rule of Three. It represents a direct relationship between two quantities where the ratio between them is constant.")
 
-    a = st.number_input("Enter value for 'a':", value=1, min_value=1, step=1)
-    b = st.number_input("Enter value for 'b':", value=1, min_value=1, step=1)
-    c = st.number_input("Enter value for 'c':", value=1, min_value=1, step=1)
+    x = st.multiselect("Select values for 'x':", options=[1, 2, 3, 4, 5])
+    y = [2, 4, 6, 8, 10]
 
-    if st.button("Calculate"):
-        x = rule_of_three_calculator(a, b, c)
-        st.write(f"The unknown value 'x' is: {x}")
-
-        visualize_rule_of_three(a, b, c, x)
+    if st.button("Visualize"):
+        visualize_proportional_relationship(x, y)
 
 if __name__ == "__main__":
     main()
