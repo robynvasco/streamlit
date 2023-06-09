9393e637f34837345808275fdf53e81baba0e9a8
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-09 12:18:15 +0200

    Update Game_rule_of_three.py

--- Game_rule_of_three.py
+++ Game_rule_of_three.py
@@ -1,49 +1,94 @@
 import streamlit as st
+import random
 
 def rule_of_three_calculator(a, b, c):
     # Calculate the unknown value using the Rule of Three
     x = (b * c) / a
     return x
 
+def generate_scenario():
+    # Generate random values for the scenario
+    a = random.randint(1, 10)
+    b = random.randint(1, 10)
+    c = random.randint(1, 10)
+
+    # Calculate the unknown value using the Rule of Three
+    x = rule_of_three_calculator(a, b, c)
+
+    return a, b, c, x
+
 def main():
-    st.title("Rule of Three Game")
+    st.title("Rule of Three Adventure")
 
-    st.write("Welcome to the Rule of Three Game! In this game, you will practice using the Rule of Three to solve problems based on proportions.")
+    st.write("Welcome to the Rule of Three Adventure! You find yourself in a mysterious land where everything revolves around proportions. Your mission is to solve the Rule of Three puzzles and find your way back home.")
 
-    st.write("Let's start with a simple example:")
-    st.write("If 2 apples cost $3, how much would 5 apples cost?")
+    while True:
+        st.write("---")
+        st.write("You encounter a wise old wizard who presents you with a challenge:")
+        
+        # Generate a scenario
+        a, b, c, x = generate_scenario()
 
-    # Get user input for the known values
-    a = 2
-    b = 3
-    c = 5
+        # Show the problem to the user
+        st.write(f"The wizard says, 'If {a} {get_object_plural(a)} cost {b} gold coins, how many gold coins would {c} {get_object_plural(c)} cost?'")
+        
+        # Get user input for their answer
+        user_answer = st.number_input("Enter your answer:", value=0, min_value=0, step=1)
 
-    # Calculate the unknown value using the Rule of Three
-    x = rule_of_three_calculator(a, b, c)
+        # Check if the user's answer is correct
+        if user_answer == int(x):
+            st.write("The wizard smiles and says, 'Well done! You have solved the puzzle. You are one step closer to finding your way home.'")
+        else:
+            st.write("The wizard shakes his head and says, 'Oh no! That's not the correct answer. Try again!'")
+            continue
 
+        st.write("---")
+        st.write("You continue on your journey and come across a group of merchants.")
 
-    # Get user input for their answer
-    user_answer = st.number_input("Enter your answer:", value=0.0, step=0.01, format="%.2f")
+        # Generate a scenario
+        a, b, c, x = generate_scenario()
 
-    # Check if the user's answer is correct
-    if abs(user_answer - x) < 0.01:
-        st.write("Congratulations! Your answer is correct.")
+        # Show the problem to the user
+        st.write(f"The merchants ask you, 'If {a} {get_object_plural(a)} cost {b} silver coins, how many silver coins would {c} {get_object_plural(c)} cost?'")
+
+        # Get user input for their answer
+        user_answer = st.number_input("Enter your answer:", value=0, min_value=0, step=1)
+
+        # Check if the user's answer is correct
+        if user_answer == int(x):
+            st.write("The merchants nod in approval and say, 'Impressive! You have solved the puzzle. You are now closer to finding your way home.'")
+        else:
+            st.write("The merchants frown and say, 'That's not the correct answer. Please try again!'")
+            continue
+
+        st.write("---")
+        st.write("As you journey further, you stumble upon a hidden treasure chest.")
+
+        # Generate a scenario
+        a, b, c, x = generate_scenario()
+
+        # Show the problem to the user
+        st.write(f"The treasure chest is locked with a secret code. The inscription reads, 'If {a} {get_object_plural(a)} cost {b} gems, how many gems would {c} {get_object_plural(c)} cost?'")
+
+        # Get user input for their answer
+        user_answer = st.number_input("Enter your answer:", value=0, min_value=0, step=1)
+
+        # Check if the user's answer is correct
+        if user_answer == int(x):
+            st.write("The treasure chest opens with a click, revealing its valuable contents. Congratulations! You have solved the final puzzle and found your way back home.")
+        else:
+            st.write("The treasure chest remains locked. The secret code must be incorrect. Keep trying!")
+
+        # Ask the user if they want to play again
+        play_again = st.button("Play Again")
+        if not play_again:
+            break
+
+def get_object_plural(number):
+    if number == 1:
+        return "item"
     else:
-        st.write(f"Oops! Your answer is incorrect. The correct answer is ${x}.")
-
-    st.write("---")
-    st.write("Let's try another example!")
-
-    # Get user input for a new set of known values
-    a = st.number_input("Enter the first number (a):", value=0.0, step=0.01, format="%.2f")
-    b = st.number_input("Enter the second number (b):", value=0.0, step=0.01, format="%.2f")
-    c = st.number_input("Enter the third number (c):", value=0.0, step=0.01, format="%.2f")
-
-    # Calculate the unknown value using the Rule of Three
-    x = rule_of_three_calculator(a, b, c)
-
-    # Show the problem and the calculated unknown value
-    st.write(f"The unknown value (x) is: {x}")
+        return "items"
 
 if __name__ == "__main__":
     main()
