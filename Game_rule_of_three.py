a9aec2d3ac7c5265db1e63c25b402184f4f35895
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-09 13:09:12 +0200

    Update Game_rule_of_three.py

--- Game_rule_of_three.py
+++ Game_rule_of_three.py
@@ -14,48 +14,56 @@ def main():
             "scenario": "Oxygen Production",
             "description": "Planet X-42 is running out of oxygen, and we need to find the right proportions of resources to save it!",
             "values": (20, 5, 50),
-            "answer": 12
+            "answer": 12,
+            "completed": False
         },
         {
             "scenario": "Water Conservation",
             "description": "Planet X-42 is facing a severe water shortage. Let's calculate how much water we need to save!",
             "values": (15, 3, 9),
-            "answer": 45
+            "answer": 45,
+            "completed": False
         }
     ]
 
-    level_index = st.sidebar.selectbox("Select a level:", range(len(levels)), format_func=lambda x: levels[x]["scenario"])
+    level_index = 0
+    while level_index < len(levels):
+        level = levels[level_index]
+        scenario = level["scenario"]
+        description = level["description"]
+        values = level["values"]
+        answer = level["answer"]
+        completed = level["completed"]
 
-    level = levels[level_index]
-    scenario = level["scenario"]
-    description = level["description"]
-    values = level["values"]
-    answer = level["answer"]
+        st.header(f"Level {level_index + 1}: {scenario}")
+        st.write(description)
 
-    st.header(f"Level {level_index + 1}: {scenario}")
-    st.write(description)
+        # Input values
+        st.subheader("Input Values:")
+        a, b, c = values
 
-    # Input values
-    st.subheader("Input Values:")
-    a, b, c = values
+        user_answer = st.number_input("How many days will it last?", value=0, min_value=0, key=f"answer_{level_index}")
 
-    user_answer = st.number_input("How many days will it last?", value=0, min_value=0, key=f"answer_{level_index}")
+        # Check answer
+        if user_answer == answer:
+            st.success("Congratulations! You provided the correct answer.")
+            level["completed"] = True
+            level_index += 1
+            if level_index < len(levels):
+                st.info("Prepare for the next level!")
+            else:
+                st.balloons()
+                st.success("You saved Planet X-42! Well done!")
 
-    # Check answer
-    if user_answer == answer:
-        st.success("Congratulations! You provided the correct answer.")
-        if level_index < len(levels) - 1:
-            st.info("Prepare for the next level!")
-        else:
-            st.balloons()
-            st.success("You saved Planet X-42! Well done!")
+        elif user_answer != 0:
+            st.error("Oops! That's not the correct answer. Try again!")
 
-    elif user_answer != 0:
-        st.error("Oops! That's not the correct answer. Try again!")
+        if not completed:
+            break
 
-    # Display result
-    st.subheader("Result:")
-    st.write(f"With {c} oxygen tanks, it will last approximately {answer} days.")
+        # Display result
+        st.subheader("Result:")
+        st.write(f"With {c} oxygen tanks, it will last approximately {answer} days.")
 
 if __name__ == "__main__":
     main()
