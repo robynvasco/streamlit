7e5eeb2aa646e26b25b220cc1bd486fbebe23f8b
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-09 12:38:40 +0200

    Update Game_rule_of_three.py

--- Game_rule_of_three.py
+++ Game_rule_of_three.py
@@ -22,33 +22,30 @@ def main():
 
     st.write("Welcome to the Rule of Three Adventure! You find yourself in a mysterious land where everything revolves around proportions. Your mission is to solve the Rule of Three puzzles and find your way back home.")
 
-    level = 1
-    while level <= 3:
+    completed_levels = []
+    while len(completed_levels) < 3:
         st.write("---")
+        level = len(completed_levels) + 1
         st.write(f"Level {level}")
 
-        if level == 1:
+        if level not in completed_levels:
             st.write("You encounter a wise old wizard who presents you with a challenge:")
-        elif level == 2:
-            st.write("You continue on your journey and come across a group of merchants.")
-        elif level == 3:
-            st.write("As you journey further, you stumble upon a hidden treasure chest.")
 
-        # Generate a scenario
-        a, b, c, x = generate_scenario()
+            # Generate a scenario
+            a, b, c, x = generate_scenario()
 
-        # Show the problem to the user
-        st.write(f"Scenario: If {a} {get_object_plural(a)} cost {b} gold coins, how many gold coins would {c} {get_object_plural(c)} cost?")
+            # Show the problem to the user
+            st.write(f"Scenario: If {a} {get_object_plural(a)} cost {b} gold coins, how many gold coins would {c} {get_object_plural(c)} cost?")
 
-        # Get user input for their answer
-        user_answer = st.number_input("Enter your answer:", key=f"level_{level}", value=0, min_value=0, step=1)
+            # Get user input for their answer
+            user_answer = st.number_input("Enter your answer:", key=f"level_{level}", value=0, min_value=0, step=1)
 
-        # Check if the user's answer is correct
-        if user_answer == int(x):
-            st.write("Correct! You have solved the puzzle.")
-            level += 1
-        else:
-            st.write("Oops! That's not the correct answer. Try again!")
+            # Check if the user's answer is correct
+            if user_answer == int(x):
+                st.write("Correct! You have solved the puzzle.")
+                completed_levels.append(level)
+            else:
+                st.write("Oops! That's not the correct answer. Try again!")
 
     st.write("---")
     st.write("Congratulations! You have successfully completed all levels and found your way back home.")
