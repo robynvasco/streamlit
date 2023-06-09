fb1c86bbc5a0e10873bdb6b91a4c1474a4349b11
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-09 12:48:14 +0200

    Update Game_rule_of_three.py

--- Game_rule_of_three.py
+++ Game_rule_of_three.py
@@ -2,35 +2,66 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 
-def proportional_relationship(x, y):
-    # Calculate the proportional relationship
-    k = y[0] / x[0]
-    return k
+def rule_of_three_calculator(a, b, c):
+    x = (b * c) / a
+    return x
 
 def visualize_proportional_relationship(x, y):
-    # Create a scatter plot to visualize the proportional relationship
     fig, ax = plt.subplots()
     ax.scatter(x, y, color='blue')
-    
-    k = proportional_relationship(x, y)
+    k = y[0] / x[0]
     ax.plot(x, k * np.array(x), color='red', linestyle='--')
-    
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_title('Proportional Relationship')
 
-    st.pyplot(fig)
+    return fig
+
+def visualize_direct_proportionality(a, b):
+    values = [a, b]
+    labels = ['a', 'b']
+    fig, ax = plt.subplots()
+    ax.bar(labels, values)
+    ax.set_xlabel('Values')
+    ax.set_ylabel('Area')
+    ax.set_title('Direct Proportionality - Area Model')
+
+    return fig
+
+def visualize_direct_proportional_slope(x, y):
+    fig, ax = plt.subplots()
+    ax.plot(x, y, marker='o')
+    slope = (y[1] - y[0]) / (x[1] - x[0])
+    ax.annotate(f"Slope = {slope}", xy=(x[0], y[0]), xytext=(0, 5), textcoords='offset points', ha='center')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_title('Direct Proportional Slope')
+
+    return fig
 
 def main():
-    st.title("Proportional Relationship Visualization")
+    st.title("Rule of Three Visualization")
 
-    st.write("The proportional relationship is a key concept in the Rule of Three. It represents a direct relationship between two quantities where the ratio between them is constant.")
+    st.write("The Rule of Three is a mathematical principle that allows you to solve problems based on proportions. Given three known values, you can calculate the unknown value using the formula: (a / b = c / x)")
 
-    x = st.multiselect("Select values for 'x':", options=[1, 2, 3, 4, 5])
-    y = [2, 4, 6, 8, 10]
+    a = st.number_input("Enter value for 'a':", value=1, min_value=1, step=1)
+    b = st.number_input("Enter value for 'b':", value=1, min_value=1, step=1)
+    c = st.number_input("Enter value for 'c':", value=1, min_value=1, step=1)
 
-    if st.button("Visualize"):
-        visualize_proportional_relationship(x, y)
+    if st.button("Calculate"):
+        x = rule_of_three_calculator(a, b, c)
+        st.write(f"The unknown value 'x' is: {x}")
+
+        x_values = [a, c]
+        y_values = [b, x]
+
+        fig1 = visualize_proportional_relationship(x_values, y_values)
+        fig2 = visualize_direct_proportionality(a, b)
+        fig3 = visualize_direct_proportional_slope(x_values, y_values)
+
+        st.pyplot(fig1)
+        st.pyplot(fig2)
+        st.pyplot(fig3)
 
 if __name__ == "__main__":
     main()
