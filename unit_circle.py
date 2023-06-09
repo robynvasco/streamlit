e1214d69b33cfd5aa2b5832a46f8be5c46a8d942
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-09 10:36:56 +0200

    Update unit_circle.py

--- unit_circle.py
+++ unit_circle.py
@@ -18,9 +18,9 @@ def plot_unit_circle(alpha, show_sine=True, show_cosine=True):
 
     plt.figure(figsize=(15, 7))
     plt.subplot(1, 2, 1)
-    plt.plot([0, x], [0, 0], '-', linewidth=3, color=custom_palette[0], label='cos' if show_cosine else None)
-    plt.plot([x, x], [0, y], '-', linewidth=3, color=custom_palette[1], label='sin' if show_sine else None)
-    plt.plot([0, x], [0, y], '-', linewidth=3, color=custom_palette[2], label='Radius')
+    plt.plot([0, x], [0, 0], '-', linewidth=3, color=custom_palette[0], label='cos(\u03B1)' if show_cosine else None)
+    plt.plot([x, x], [0, y], '-', linewidth=3, color=custom_palette[1], label='sin(\u03B1)' if show_sine else None)
+    plt.plot([0, x], [0, y], '-', linewidth=3, color=custom_palette[2], label='Radius=1')
     plt.plot(np.cos(np.linspace(0, 2*np.pi, 100)), np.sin(np.linspace(0, 2*np.pi, 100)), '--', linewidth=1, color='gray', alpha=0.5, label='Unit Circle')
     plt.xlim(-1.5, 1.5)
     plt.ylim(-1.5, 1.5)
@@ -33,19 +33,19 @@ def plot_unit_circle(alpha, show_sine=True, show_cosine=True):
     plt.gca().set_aspect('equal')  # Set aspect ratio to equal
 
     # Draw floating labels for sin and cos lines
-    plt.text(x/2, -0.1, 'cos', ha='center', va='bottom', fontsize=12, color=custom_palette[0] if show_cosine else 'none')
-    plt.text(x+0.1, y/2, 'sin', ha='center', va='bottom', fontsize=12, color=custom_palette[1] if show_sine else 'none')
+    plt.text(x/2, -0.1, 'cos(\u03B1)', ha='center', va='bottom', fontsize=12, color=custom_palette[0] if show_cosine else 'none')
+    plt.text(x+0.1, y/2, 'sin(\u03B1)', ha='center', va='bottom', fontsize=12, color=custom_palette[1] if show_sine else 'none')
 
     # Draw the angle arc and annotate the angle value in degrees
     angle_degrees = np.degrees(alpha)
-    arc = Arc((0, 0), 1, 1, 0, 0, angle_degrees, color="grey", linewidth=2)
+    arc = Arc((0, 0), 1, 1, 0, 0, theta1=0, theta2=angle_degrees, color="grey", linewidth=2)
     plt.gca().add_patch(arc)
     angle_text = f"\u03B1={int(angle_degrees)}\u00B0"
     plt.text(0.5, 0.15, angle_text, ha='center', va='center', fontsize=10, color='black')
 
-    # Plot the sine and cos function
-    plt.subplot(1, 2, 2)
+    # Plot the sine and cosine functions
     if show_sine:
+        plt.subplot(1, 2, 2)
         plt.plot(np.linspace(0, 2*np.pi, 100), np.sin(np.linspace(0, 2*np.pi, 100)), color=custom_palette[1], linewidth=2)
         plt.scatter(alpha, y, color=custom_palette[1], s=50)
         plt.plot([alpha, alpha], [0, y], '-', color=custom_palette[1], linewidth=3)
@@ -58,8 +58,10 @@ def plot_unit_circle(alpha, show_sine=True, show_cosine=True):
     tick_values = np.linspace(0, 2*np.pi, 5)
     tick_labels = ['0', 'π/2', 'π', '3π/2', '2π']
     plt.xticks(tick_values, tick_labels)
-    plt.yticks([-1, -0.5, 0, 0.5, 1])
-    plt.xlabel('\u03B1')
+    tick_values = np.linspace(-1,-np.sqrt(3)/2,-np.sqrt(2)/2,-1/2,0,1/2,np.sqrt(2)/2,np.sqrt(3)/2,1)
+    tick_labels = ['-1','-√3/2','-√2/2','-½', '0', '½', '√2/2', '√3/2', '1']
+    plt.yticks(tick_values, tick_labels)
+    plt.xlabel('x')
     plt.ylabel('y')
     plt.ylim(-1.5, 1.5)
     plt.title('Sine and Cosine Functions', fontsize=18, fontweight='bold')
@@ -72,20 +74,20 @@ def main():
     st.title('Unit Circle and Trigonometric Functions')
     alpha = st.slider('Select the angle \u03B1 in \u00B0', 0, 360, 45)
     alpha_rad = np.radians(alpha)
-    
-    show_sine = st.checkbox('Show Sine')
-    show_cosine = st.checkbox('Show Cosine')
-    
+
+    show_sine = st.checkbox('Show Sine', value=True)
+    show_cosine = st.checkbox('Show Cosine', value=True)
+
     plot_unit_circle(alpha_rad, show_sine, show_cosine)
-    
+
     st.write('')
     st.write('### Trigonometric Values')
     table_data = {
         'α in degrees': ['0°', '30°', '45°', '60°', '90°'],
         'α in radians': ['0', 'π/6', 'π/4', 'π/3', 'π/2'],
-        'sin(α)': ['0', '½', '½√2', '½√3', '1'],
-        'Memory Aid for sin(α)': ['½√0', '½√1', '½√2', '½√3', '½√4'],
-        'cos(α)': ['1', '½√3', '½√2', '½', '0']
+        'sin(α)': ['0', '½', '√2/2', '√3/2', '1'],
+        'Memory Aid for sin(α)': ['√0/2', '√1/2', '√2/2', '√3/2', '√4/2'],
+        'cos(α)': ['1', '√3/2', '√2/2', '½', '0']
     }
 
     df = pd.DataFrame(table_data)
