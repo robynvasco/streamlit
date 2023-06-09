d201aa008799558698ffd38da70df022b93c2f52
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-09 12:37:22 +0200

    Update Game_rule_of_three.py

--- Game_rule_of_three.py
+++ Game_rule_of_three.py
@@ -22,68 +22,36 @@ def main():
 
     st.write("Welcome to the Rule of Three Adventure! You find yourself in a mysterious land where everything revolves around proportions. Your mission is to solve the Rule of Three puzzles and find your way back home.")
 
-    play_again = True
-    key = f"{random.randint(1, 1000)}"
-    while play_again:
+    level = 1
+    while level <= 3:
         st.write("---")
-        st.write("You encounter a wise old wizard who presents you with a challenge:")
-        
-        # Generate a scenario
-        a, b, c, x = generate_scenario()
+        st.write(f"Level {level}")
 
-        # Show the problem to the user
-        st.write(f"The wizard says, 'If {a} {get_object_plural(a)} cost {b} gold coins, how many gold coins would {c} {get_object_plural(c)} cost?'")
-        
-        # Get user input for their answer
-        user_answer = st.number_input("Enter your answer:", key=f"wizard_{key}", value=0, min_value=0, step=1)
-
-        # Check if the user's answer is correct
-        if user_answer == int(x):
-            st.write("The wizard smiles and says, 'Well done! You have solved the puzzle. You are one step closer to finding your way home.'")
-        else:
-            st.write("The wizard shakes his head and says, 'Oh no! That's not the correct answer. Try again!'")
-            continue
-
-        st.write("---")
-        st.write("You continue on your journey and come across a group of merchants.")
+        if level == 1:
+            st.write("You encounter a wise old wizard who presents you with a challenge:")
+        elif level == 2:
+            st.write("You continue on your journey and come across a group of merchants.")
+        elif level == 3:
+            st.write("As you journey further, you stumble upon a hidden treasure chest.")
 
         # Generate a scenario
         a, b, c, x = generate_scenario()
 
         # Show the problem to the user
-        st.write(f"The merchants ask you, 'If {a} {get_object_plural(a)} cost {b} silver coins, how many silver coins would {c} {get_object_plural(c)} cost?'")
+        st.write(f"Scenario: If {a} {get_object_plural(a)} cost {b} gold coins, how many gold coins would {c} {get_object_plural(c)} cost?")
 
         # Get user input for their answer
-        user_answer = st.number_input("Enter your answer:", key=f"merchants_{key}", value=0, min_value=0, step=1)
+        user_answer = st.number_input("Enter your answer:", key=f"level_{level}", value=0, min_value=0, step=1)
 
         # Check if the user's answer is correct
         if user_answer == int(x):
-            st.write("The merchants nod in approval and say, 'Impressive! You have solved the puzzle. You are now closer to finding your way home.'")
+            st.write("Correct! You have solved the puzzle.")
+            level += 1
         else:
-            st.write("The merchants frown and say, 'That's not the correct answer. Please try again!'")
-            continue
+            st.write("Oops! That's not the correct answer. Try again!")
 
-        st.write("---")
-        st.write("As you journey further, you stumble upon a hidden treasure chest.")
-
-        # Generate a scenario
-        a, b, c, x = generate_scenario()
-
-        # Show the problem to the user
-        st.write(f"The treasure chest is locked with a secret code. The code is based on the Rule of Three. The inscription reads,"
-                 f"'If {a} {get_object_plural(a)} cost {b} gems, how many gems would {c} {get_object_plural(c)} cost?'")
-
-        # Get user input for their answer
-        user_answer = st.number_input("Enter your answer:", key=f"treasure_{key}", value=0, min_value=0, step=1)
-
-        # Check if the user's answer is correct
-        if user_answer == int(x):
-            st.write("The treasure chest opens with a click, revealing its valuable contents. Congratulations! You have solved the final puzzle and found your way back home.")
-        else:
-            st.write("The treasure chest remains locked. The secret code must be incorrect. Keep trying!")
-
-        # Ask the user if they want to play again
-        play_again = st.button("Play Again")
+    st.write("---")
+    st.write("Congratulations! You have successfully completed all levels and found your way back home.")
 
 def get_object_plural(number):
     if number == 1:
