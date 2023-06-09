1d7e4e2eb4cb8eaaa5799f1906fde0905680e698
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-09 11:35:24 +0200

    Update unit_circle.py

--- unit_circle.py
+++ unit_circle.py
@@ -18,8 +18,8 @@ def plot_unit_circle(alpha, show_sine=True, show_cosine=True):
 
     plt.figure(figsize=(15, 7))
     plt.subplot(1, 2, 1)
-    plt.plot([0, x], [0, 0], '-', linewidth=3, color=custom_palette[0], label='cos(α)' if show_cosine else None)
-    plt.plot([x, x], [0, y], '-', linewidth=3, color=custom_palette[1], label='sin(α)' if show_sine else None)
+    plt.plot([0, x], [0, 0], '-', linewidth=3, color=custom_palette[0], label=r'$\cos(\alpha)$' if show_cosine else None)
+    plt.plot([x, x], [0, y], '-', linewidth=3, color=custom_palette[1], label=r'$\sin(\alpha)$' if show_sine else None)
     plt.plot([0, x], [0, y], '-', linewidth=3, color=custom_palette[2], label='Radius=1')
     plt.plot(np.cos(np.linspace(0, 2*np.pi, 100)), np.sin(np.linspace(0, 2*np.pi, 100)), '--', linewidth=1, color='gray', alpha=0.5, label='Unit Circle')
     plt.xlim(-1.5, 1.5)
@@ -33,41 +33,37 @@ def plot_unit_circle(alpha, show_sine=True, show_cosine=True):
     plt.gca().set_aspect('equal')  # Set aspect ratio to equal
 
     # Draw floating labels for sin and cos lines
-    plt.text(x/2, -0.1, 'cos(α)', ha='center', va='bottom', fontsize=12, color=custom_palette[0] if show_cosine else 'none')
-    plt.text(x+0.2, y/2, 'sin(α)', ha='center', va='bottom', fontsize=12, color=custom_palette[1] if show_sine else 'none')
+    plt.text(x/2, -0.1, r'$\cos(\alpha)$', ha='center', va='bottom', fontsize=12, color=custom_palette[0] if show_cosine else 'none')
+    plt.text(x+0.2, y/2, r'$\sin(\alpha)$', ha='center', va='bottom', fontsize=12, color=custom_palette[1] if show_sine else 'none')
 
     # Draw the angle arc and annotate the angle value in degrees
     angle_degrees = np.degrees(alpha)
     arc = Arc((0, 0), 1, 1, 0, 0, angle_degrees, color="grey", linewidth=2)
     plt.gca().add_patch(arc)
-    angle_text = f"α={round(angle_degrees)}°"
-    plt.text(0.3, 0.07, angle_text, ha='center', va='center', fontsize=10, color='grey')
+    angle_text = r'$\alpha={}$'.format(int(angle_degrees)) + r'$^\circ$'
+    plt.text(0.3, 0.15, angle_text, ha='center', va='center', fontsize=10, color='grey')
 
     ######### Plot the sine and cosine functions
-    if show_sine or show_cosine:
+    if show_sine:
         plt.subplot(1, 2, 2)
-        if show_sine:
-            plt.plot(np.linspace(0, 2*np.pi, 100), np.sin(np.linspace(0, 2*np.pi, 100)), color=custom_palette[1], linewidth=2)
-            plt.scatter(alpha, y, color=custom_palette[1], s=50)
-            plt.plot([alpha, alpha], [0, y], '-', color=custom_palette[1], linewidth=3)
-        if show_cosine:
-            plt.plot(np.linspace(0, 2*np.pi, 100), np.cos(np.linspace(0, 2*np.pi, 100)), color=custom_palette[0], linewidth=2)
-            plt.scatter(alpha, x, color=custom_palette[0], s=50)
-            plt.plot([alpha, alpha], [0, x], '-', color=custom_palette[0], linewidth=2)
-    else:
-        plt.subplot(1, 2, 2)
-        plt.axis('off')
+        plt.plot(np.linspace(0, 2*np.pi, 100), np.sin(np.linspace(0, 2*np.pi, 100)), color=custom_palette[1], linewidth=2)
+        plt.scatter(alpha, y, color=custom_palette[1], s=50)
+        plt.plot([alpha, alpha], [0, y], '-', color=custom_palette[1], linewidth=3)
+
+    if show_cosine:
+        plt.plot(np.linspace(0, 2*np.pi, 100), np.cos(np.linspace(0, 2*np.pi, 100)), color=custom_palette[0], linewidth=2)
+        plt.scatter(alpha, x, color=custom_palette[0], s=50)
+        plt.plot([alpha, alpha], [0, x], '-', color=custom_palette[0], linewidth=2)
 
     tick_values = np.linspace(0, 2*np.pi, 5)
-    tick_labels = ['0', 'π/2', 'π', '3π/2', '2π']
+    tick_labels = ['0', r'$\frac{\pi}{2}$', r'$\pi$', r'$\frac{3\pi}{2}$', r'$2\pi$']
     plt.xticks(tick_values, tick_labels)
     tick_values = np.array([-1,-np.sqrt(3)/2,-np.sqrt(2)/2,-1/2,0,1/2,np.sqrt(2)/2,np.sqrt(3)/2,1])
-    tick_labels = ['-1','-√3/2','-√2/2','-½', '0', '½', '√2/2', '√3/2', '1']
+    tick_labels = ['-1', r'$-\frac{\sqrt{3}}{2}$', r'$-\frac{\sqrt{2}}{2}$', r'$-\frac{1}{2}$', '0', r'$\frac{1}{2}$', r'$\frac{\sqrt{2}}{2}$', r'$\frac{\sqrt{3}}{2}$', '1']
     plt.yticks(tick_values, tick_labels)
     plt.xlabel('x')
     plt.ylabel('y')
     plt.ylim(-1.5, 1.5)
-    plt.xlim(0, 2*np.pi)
     plt.title('Sine and Cosine Functions', fontsize=18, fontweight='bold')
     plt.grid(True, linestyle='--', linewidth=0.5)
 
@@ -76,14 +72,11 @@ def plot_unit_circle(alpha, show_sine=True, show_cosine=True):
 
 def main():
     st.title('Unit Circle and Trigonometric Functions')
-    st.write('### Why use the Unit Circle?')
-    st.write('The unit circle is a circle with a radius of 1 unit and its center at the origin (0, 0) of a coordinate plane. It is widely used in trigonometry to understand the relationships between sine and cosine values at different angles. Here are a few reasons why we use the unit circle:')
-
-    alpha = st.slider('Select the angle α in °', 0, 360, 45)
+    alpha = st.slider('Select the angle α in degrees', 0, 360, 45)
     alpha_rad = np.radians(alpha)
 
-    show_sine = st.checkbox('Show sin(α)', value=True)
-    show_cosine = st.checkbox('Show cos(α)', value=True)
+    show_sine = st.checkbox('Show Sine', value=True)
+    show_cosine = st.checkbox('Show Cosine', value=True)
 
     plot_unit_circle(alpha_rad, show_sine, show_cosine)
 
@@ -91,10 +84,10 @@ def main():
     st.write('### Trigonometric Values')
     table_data = {
         'α in degrees': ['0°', '30°', '45°', '60°', '90°'],
-        'α in radians': ['0', 'π/6', 'π/4', 'π/3', 'π/2'],
-        'sin(α)': ['0', '½', '√2/2', '√3/2', '1'],
-        'Memory Aid for sin(α)': ['√0/2', '√1/2', '√2/2', '√3/2', '√4/2'],
-        'cos(α)': ['1', r'$\frac{\pi}{6}$', '√2/2', '½', '0']
+        'α in radians': ['0', r'$\frac{\pi}{6}$', r'$\frac{\pi}{4}$', r'$\frac{\pi}{3}$', r'$\frac{\pi}{2}$'],
+        'sin(α)': ['0', r'$\frac{1}{2}$', r'$\frac{\sqrt{2}}{2}$', r'$\frac{\sqrt{3}}{2}$', '1'],
+        'Memory Aid for sin(α)': [r'$\sqrt{0}/2$', r'$\sqrt{1}/2$', r'$\sqrt{2}/2$', r'$\sqrt{3}/2$', r'$\sqrt{4}/2$'],
+        'cos(α)': ['1', r'$\frac{\sqrt{3}}{2}$', r'$\frac{\sqrt{2}}{2}$', r'$\frac{1}{2}$', '0']
     }
 
     df = pd.DataFrame(table_data)
