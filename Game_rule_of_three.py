b5dc7b07b1f28c8b97fadb98e17d47946e986de1
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-09 12:55:53 +0200

    Update Game_rule_of_three.py

--- Game_rule_of_three.py
+++ Game_rule_of_three.py
@@ -1,41 +1,14 @@
 import streamlit as st
 import matplotlib.pyplot as plt
-import numpy as np
 
-def rule_of_three_calculator(a, b, c):
-    x = (b * c) / a
-    return x
-
-def visualize_proportional_relationship(x, y):
+def visualize_direct_proportionality(a, b, c, x):
+    areas = [a, b, c, x]
+    labels = ['a', 'b', 'c', 'x']
     fig, ax = plt.subplots()
-    ax.scatter(x, y, color='blue')
-    k = y[0] / x[0]
-    ax.plot(x, k * np.array(x), color='red', linestyle='--')
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_title('Proportional Relationship')
-
-    return fig
-
-def visualize_direct_proportionality(a, b):
-    values = [a, b]
-    labels = ['a', 'b']
-    fig, ax = plt.subplots()
-    ax.bar(labels, values)
+    ax.bar(labels, areas)
     ax.set_xlabel('Values')
     ax.set_ylabel('Area')
     ax.set_title('Direct Proportionality - Area Model')
-
-    return fig
-
-def visualize_direct_proportional_slope(x, y):
-    fig, ax = plt.subplots()
-    ax.plot(x, y, marker='o')
-    slope = (y[1] - y[0]) / (x[1] - x[0])
-    ax.annotate(f"Slope = {slope}", xy=(x[0], y[0]), xytext=(0, 5), textcoords='offset points', ha='center')
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_title('Direct Proportional Slope')
 
     return fig
 
@@ -49,19 +22,11 @@ def main():
     c = st.number_input("Enter value for 'c':", value=1, min_value=1, step=1)
 
     if st.button("Calculate"):
-        x = rule_of_three_calculator(a, b, c)
+        x = (b * c) / a
         st.write(f"The unknown value 'x' is: {x}")
 
-        x_values = [a, c]
-        y_values = [b, x]
+        fig2 = visualize_direct_proportionality(a, b, c, x)
 
-        fig1 = visualize_proportional_relationship(x_values, y_values)
-        fig2 = visualize_direct_proportionality(a, b)
-        fig3 = visualize_direct_proportional_slope(x_values, y_values)
-
-        st.pyplot(fig1)
         st.pyplot(fig2)
-        st.pyplot(fig3)
 
-if __name__ == "__main__":
-    main()
+if __name__
