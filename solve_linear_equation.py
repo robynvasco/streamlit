4f6216b22681839478a0fd082f43df7d8c595833
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-11 11:37:54 +0200

    Update solve_linear_equation.py

--- solve_linear_equation.py
+++ solve_linear_equation.py
@@ -1,12 +1,11 @@
 import streamlit as st
 from sympy import Symbol, Eq, parse_expr, latex
 
-
 def apply_term_to_equation(term, equation):
     x = Symbol('x')
     left_side, right_side = equation.args
-    new_left_side = left_side + term
-    new_right_side = right_side + term
+    new_left_side = parse_expr(str(left_side) + term)
+    new_right_side = parse_expr(str(right_side) + term)
     new_equation = Eq(new_left_side, new_right_side)
     return new_equation
 
@@ -20,7 +19,7 @@ def main():
     st.latex(latex(st.session_state['equation']))
 
     term = st.text_input("Enter a term to apply to the equation (e.g., +1 or *2/3):")
-    term = parse_expr(term) if term else 0
+    term = str(term) if term else ""
 
     if st.button("Apply Term"):
         equation = apply_term_to_equation(term, st.session_state['equation'])
