d0abfc55b8fbe1d8bbd6c444356dcb134cff54ef
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-11 10:29:11 +0200

    Update solve_linear_equation.py

--- solve_linear_equation.py
+++ solve_linear_equation.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from sympy import Symbol, Eq, simplify, pprint
+from sympy import Symbol, Eq, simplify, latex
 
 def apply_term_to_equation(term, equation):
     x = Symbol('x')
@@ -15,7 +15,7 @@ def main():
     equation = Eq((Symbol('x') + 3) / 15 + 3, 5)
 
     st.markdown("Original Equation:")
-    st.latex(str(pprint(equation, use_unicode=True)))
+    st.latex(latex(equation))
 
     term = st.text_input("Enter a term to apply to the equation (e.g., +1 or *2/3):")
     term = eval(term) if term else 0
@@ -23,7 +23,7 @@ def main():
     if st.button("Apply Term"):
         equation = apply_term_to_equation(term, equation)
         st.markdown("Updated Equation:")
-        st.latex(str(pprint(equation, use_unicode=True)))
+        st.latex(latex(equation))
 
 if __name__ == "__main__":
     main()
