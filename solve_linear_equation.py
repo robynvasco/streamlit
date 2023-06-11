a17aba873452493390ec9aa5b2a42159b8fc4e26
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-11 14:05:50 +0200

    Update solve_linear_equation.py

--- solve_linear_equation.py
+++ solve_linear_equation.py
@@ -11,23 +11,28 @@ def apply_term_to_equation(term, equation):
 
 def main():
     st.title("Equation Manipulator")
-    start_equation=Eq((Symbol('x') + 3) / 15 + 3, 5)
-    
-    st.markdown("Original Equation:")
-    st.latex(latex(start_equation))
 
     if 'equation' not in st.session_state:
-        st.session_state['equation'] = start_equation
-    
-    if st.button("Apply Term"):
+        st.session_state['equation'] = Eq((Symbol('x') + 3) / 15 + 3, 5)
+
+    original_eq_container = st.container()
+    updated_eq_container = st.container()
+    input_container = st.container()
+
+    with original_eq_container:
+        st.markdown("Original Equation:")
+        st.latex(latex(st.session_state['equation']))
+
+    term = input_container.text_input("Enter a term to apply to the equation (e.g., +1 or *2/3):")
+    term = str(term) if term else ""
+
+    if input_container.button("Apply Term"):
         equation = apply_term_to_equation(term, st.session_state['equation'])
         st.session_state['equation'] = equation
-        
-    st.latex(latex(st.session_state['equation']))
 
-    term = st.text_input("Enter a term to apply to the equation (e.g., +1 or *2/3):")
-    term = str(term) if term else ""
-   
+    with updated_eq_container:
+        st.markdown("Updated Equation:")
+        st.latex(latex(st.session_state['equation']))
 
 if __name__ == "__main__":
     main()
