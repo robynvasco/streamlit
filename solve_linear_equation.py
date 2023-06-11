a45571c317df08909a828ddf2646b0cf5e541da6
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-11 10:36:24 +0200

    Update solve_linear_equation.py

--- solve_linear_equation.py
+++ solve_linear_equation.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from sympy import Symbol, Eq, simplify, latex
+from streamlit.hashing import _CodeHasher
 
 def apply_term_to_equation(term, equation):
     x = Symbol('x')
@@ -12,20 +13,21 @@ def apply_term_to_equation(term, equation):
 def main():
     st.title("Equation Manipulator")
 
-    equation = Eq((Symbol('x') + 3) / 15 + 3, 5)
+    session_state = st.session_state.get(hash(_CodeHasher.hash("state")), {})
+    if 'equation' not in session_state:
+        session_state['equation'] = Eq((Symbol('x') + 3) / 15 + 3, 5)
 
     st.markdown("Original Equation:")
-    st.latex(latex(equation))
+    st.latex(latex(session_state['equation']))
 
     term = st.text_input("Enter a term to apply to the equation (e.g., +1 or *2/3):")
     term = eval(term) if term else 0
 
     if st.button("Apply Term"):
-        equation = apply_term_to_equation(term, equation)
+        equation = apply_term_to_equation(term, session_state['equation'])
+        session_state['equation'] = equation
         st.markdown("Updated Equation:")
         st.latex(latex(equation))
 
 if __name__ == "__main__":
     main()
-
-    
