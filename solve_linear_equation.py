3255bad69b2791ce10969cc7f5993ff233ef8767
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-11 19:34:39 +0200

    Update solve_linear_equation.py

--- solve_linear_equation.py
+++ solve_linear_equation.py
@@ -12,6 +12,7 @@ def apply_term_to_equation(term, equation):
 def undo_last_action():
     if len(st.session_state['equations']) > 1:
         st.session_state['equations'] = st.session_state['equations'][:-1]
+    print("Equation List:", st.session_state['equations'])
 
 def main():
     st.title("Equation Manipulator")
@@ -26,7 +27,7 @@ def main():
     col1, col2, col3 = st.columns([3, 1, 1])
 
     term = col1.text_input(
-        "Write term to add",
+        "b",
         label_visibility="collapsed",
         disabled=False,
         placeholder="e.g., +1 or *(1/2)",
