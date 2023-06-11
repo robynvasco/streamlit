139a9940fb27e794f967e30b62aea3c5193ebf5c
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-11 10:01:45 +0200

    Update solve_linear_equation.py

--- solve_linear_equation.py
+++ solve_linear_equation.py
@@ -1,32 +1,25 @@
 import streamlit as st
-from sympy import Symbol, Eq, simplify
+import sympy as sp
 
-def apply_term_to_equation(equation, term, operator):
-    x = Symbol('x')
-    equation_term = f'{term} {operator} 0'
-    term_expr = eval(equation_term)
-    new_equation = Eq(simplify(equation.lhs + term_expr), simplify(equation.rhs + term_expr))
+def apply_term(equation, term):
+    x = sp.symbols('x')
+    new_equation = equation + term
+    new_equation = sp.simplify(new_equation)
     return new_equation
 
 def main():
     st.title("Equation Manipulator")
-    st.write("Start with the equation (x+3)/15+3 = 5")
+    equation = sp.Eq((sp.symbols('x') + 3) / 15 + 3, 5)
+    
+    st.subheader("Initial Equation:")
+    st.latex(sp.latex(equation))
 
-    # Initial equation
-    equation = Eq((Symbol('x') + 3) / 15 + 3, 5)
-
-    # User input
-    term_input = st.text_input("Enter a term:")
-
-    # Choose operator
-    operator = st.selectbox("Choose an operator:", ('+', '-', '*', '/'))
-
-    # Apply term to equation
+    term = st.text_input("Enter a term to apply to both sides of the equation:")
     if st.button("Apply Term"):
-        new_equation = apply_term_to_equation(equation, term_input, operator)
-        equation = new_equation
-
-    st.write(f"Equation: {equation.lhs} = {equation.rhs}")
+        if term:
+            equation = apply_term(equation, term)
+            st.subheader("Updated Equation:")
+            st.latex(sp.latex(equation))
 
 if __name__ == "__main__":
     main()
