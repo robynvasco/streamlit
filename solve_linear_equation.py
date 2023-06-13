22f83c6a6fd0c1eee9849a6853e30c955f2ca3c2
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-13 13:06:46 +0200

    Update solve_linear_equation.py

--- solve_linear_equation.py
+++ solve_linear_equation.py
@@ -88,7 +88,11 @@ def main():
             with term_col:
                 term = terms[i] if i < len(terms) else ''
                 term_text = f"|    {term.replace('*', '⋅')}"
-                st.latex(latex(f"\cdot"))
+                st.latex(r'''
+                a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+                \sum_{k=0}^{n-1} ar^k =
+                a \left(\frac{1-r^{n}}{1-r}\right)
+                ''')
 
     
 
