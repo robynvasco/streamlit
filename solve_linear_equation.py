19ddf1e126ef5bc6414ed7d9010a0261f7923eab
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-13 21:27:36 +0200

    Update solve_linear_equation.py

--- solve_linear_equation.py
+++ solve_linear_equation.py
@@ -7,11 +7,11 @@ import random
 def apply_term_to_equation(term, equation):
     x = Symbol('x')
     left_side, right_side = equation.args
-   
+
     if "()" in term:
         st.error("Invalid term. Empty parentheses.")
         return equation
-      
+
     try:
         new_left_side = sympify(f"({left_side}){term}")
         new_right_side = sympify(f"({right_side}){term}")
@@ -42,7 +42,7 @@ def start_new_game():
     equation_database = [
         Eq((Symbol('x') + 3) / 15 + 3, 5),
         Eq((Symbol('x') - 2) * 4 - 10, 6),
-        Eq((Symbol('x')* 2 + 5) / 2, 8),
+        Eq((Symbol('x') ** 2 + 5) / 2, 8),
         # Add more equations to the database
     ]
     random_equation = random.choice(equation_database)
@@ -62,7 +62,7 @@ def main():
 
     # Create a column layout
     col1, col2, col3 = st.columns([3, 1, 1])
-    
+
     term = col1.text_input(
         "b",
         label_visibility="collapsed",
@@ -72,7 +72,7 @@ def main():
 
     apply_clicked = False
 
-    if col2.button("Apply Term"):
+    if col2.button("Apply Term", key="apply"):
         apply_clicked = True
 
     if term and not apply_clicked:
@@ -81,15 +81,15 @@ def main():
         if equation != st.session_state['equations'][-1]:
             st.session_state['equations'].append(equation)
             st.session_state['terms'].append(term)
-            
+
             # Check if x is isolated
             if equation.lhs == Symbol('x'):
                 st.balloons()
                 st.success("Congratulations! You have isolated x and found the solution!")
-                st.button("Click here to begin a new game", on_click=start_new_game)
+                st.button("Click here to begin a new game", key="new_game", on_click=start_new_game)
 
     if len(st.session_state['equations']) > 1:
-        if col3.button("Undo"):
+        if col3.button("Undo", key="undo"):
             undo_last_action()
 
     # Display the updated equations and applied terms
