cad413d1b9072553cf4b8f39a2f7db902e082713
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-13 10:21:39 +0200

    Update solve_linear_equation.py

--- solve_linear_equation.py
+++ solve_linear_equation.py
@@ -3,19 +3,22 @@ from sympy import Symbol, Eq, parse_expr, latex, SympifyError, sympify
 import re
 
 
+from sympy.parsing.sympy_parser import parse_expr
+
 def apply_term_to_equation(term, equation):
     x = Symbol('x')
     left_side, right_side = equation.args
     try:
         if term.count('(') != term.count(')'):
             raise SympifyError("Mismatched parentheses")
-        new_left_side = sympify(f"({left_side}){term}")
-        new_right_side = sympify(f"({right_side}){term}")
+        new_left_side = parse_expr(f"({left_side}){term}")
+        new_right_side = parse_expr(f"({right_side}){term}")
         new_equation = Eq(new_left_side, new_right_side)
         return new_equation
     except SympifyError:
         st.error("Invalid term. Please check the syntax.")
         return equation
+
 
 
 def undo_last_action():
