ecf0115129f66511027da8e96e46b658c21be4f6
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-13 21:55:17 +0200

    Update solve_linear_equation.py

--- solve_linear_equation.py
+++ solve_linear_equation.py
@@ -23,7 +23,6 @@ def apply_term_to_equation(term, equation):
 
 
 def undo_last_action():
-    if len(st.session_state['equations']) > 1:
         st.session_state['equations'] = st.session_state['equations'][:-1]
         st.session_state['terms'] = st.session_state['terms'][:-1]
         
@@ -77,7 +76,7 @@ def main():
     if col2.button("Apply Term", key="apply"):
         apply_clicked = True
         
-    if len(st.session_state['terms']) > 1:
+    if len(st.session_state['equations']) > 1:
         if col3.button("Undo", key="undo"):
             undo_triggered = True
             undo_last_action()
