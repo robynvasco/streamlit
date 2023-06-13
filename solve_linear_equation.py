9fd0d68eb6c80532b5f0f9a000c2640e59869ae0
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-13 12:01:32 +0200

    Update solve_linear_equation.py

--- solve_linear_equation.py
+++ solve_linear_equation.py
@@ -3,7 +3,6 @@ from sympy import Symbol, Eq, parse_expr, latex, SympifyError, sympify
 import re
 
 
-
 def apply_term_to_equation(term, equation):
     x = Symbol('x')
     left_side, right_side = equation.args
@@ -18,30 +17,31 @@ def apply_term_to_equation(term, equation):
         new_equation = Eq(new_left_side, new_right_side)
         return new_equation
     except SympifyError:
-        st.error("Invalid term. Please check the syntax and mismatched parantheses.")
+        st.error("Invalid term. Please check the syntax and mismatched parentheses.")
         return equation
 
 
-
 def undo_last_action():
-    if len(st.session_state['equations']) > 2:
-        st.session_state['equations'] = st.session_state['equations'][:-2]
+    if len(st.session_state['equations']) > 1:
+        st.session_state['equations'] = st.session_state['equations'][:-1]
         st.write(st.session_state['equations'])
+
 
 def add_multiplication_operator(match):
     return match.group(1) + '*' + match.group(2)
+
 
 def insert_multiplication_operators(term):
     term = re.sub(r'(\d)([a-zA-Z\(])', add_multiplication_operator, term)
     term = re.sub(r'(\))(?=[a-zA-Z])', add_multiplication_operator, term)
     return term
 
+
 def main():
     st.title("Equation Manipulator")
 
     if 'equations' not in st.session_state:
         st.session_state['equations'] = [Eq((Symbol('x') + 3) / 15 + 3, 5)]
-    if 'terms' not in st.session_state:
         st.session_state['terms'] = []
 
     st.info("Isolate x for the following equation")
@@ -74,17 +74,19 @@ def main():
         if col3.button("Undo"):
             undo_last_action()
 
-    # Display the updated equations and terms
+    # Display the updated equations
     with original_eq_container:
         equations = st.session_state['equations']
         terms = st.session_state['terms']
-        for i, (equation, term) in enumerate(zip(equations, terms)):
+        for i, equation in enumerate(equations):
             st.latex(latex(equation))
-            col4.write(f"Term: {term}")
             if i < len(equations) - 1:
                 st.markdown("---")
-          
-        
+        if terms:
+            st.write("Applied Terms:")
+            for term in terms:
+                st.write(term)
+
+
 if __name__ == "__main__":
     main()
-
