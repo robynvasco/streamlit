fba119e74d24e6f16fb8a4c0422d64ac5ce2ccb4
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-16 12:35:07 +0200

    up

--- solve_linear_equation.py
+++ solve_linear_equation.py
@@ -1,3 +1,58 @@
+import streamlit as st
+from sympy import Symbol, Eq, parse_expr, latex, SympifyError, sympify
+import re
+import random
+
+def enter():
+    st.session_state.saved_input = st.session_state.input
+    st.session_state.input = ''
+
+def clear_text():
+    st.session_state.input = ''
+
+def apply_term(new_term, level):
+    term = insert_multiplication_operators(new_term)
+    equation = apply_term_to_equation(term, st.session_state['equations'][-1])
+    if equation != st.session_state['equations'][-1]:
+        st.session_state['equations'].append(equation)
+        st.session_state['terms'].append(term)
+
+        # Check if x is isolated
+        if equation.lhs == Symbol('x'):
+            st.balloons()
+            st.success("Congratulations! You have isolated x and found the solution!")
+            st.button("Click here to begin a new game", key="new_game", on_click=lambda: start_new_game(level))
+
+def apply_term_to_equation(term, equation):
+    x = Symbol('x')
+    left_side, right_side = equation.args
+
+    if "()" in term:
+        st.error("Invalid term. Empty parentheses.")
+        return equation
+
+    try:
+        new_left_side = sympify(f"({left_side}){term}")
+        new_right_side = sympify(f"({right_side}){term}")
+        new_equation = Eq(new_left_side, new_right_side)
+        return new_equation
+    except SympifyError:
+        st.error("Invalid term. Please check the syntax and mismatched parentheses.")
+        return equation
+
+def undo_last_action():
+    if len(st.session_state['equations']) > 1:
+        st.session_state['equations'] = st.session_state['equations'][:-1]
+        st.session_state['terms'] = st.session_state['terms'][:-1]
+
+def add_multiplication_operator(match):
+    return match.group(1) + '*' + match.group(2)
+
+def insert_multiplication_operators(term):
+    term = re.sub(r'(\d)([a-zA-Z\(])', add_multiplication_operator, term)
+    term = re.sub(r'(\))(?=[a-zA-Z])', add_multiplication_operator, term)
+    return term
+
 def start_new_game(level):
     st.session_state.saved_input = ""
     equation_databases = {
@@ -34,11 +89,12 @@ def main():
     # Create a column layout
     col1, col2, col3 = st.columns([3, 1, 1])
 
-    level = st.sidebar.selectbox("Select Level", ["Level 1", "Level 2"], key="level")  # Add more levels
-
+    level = st.sidebar.selectbox("Select Level", ["Level 1", "Level 2"])  # Add more levels
+    
     if st.session_state.level != level:
         start_new_game(level)
         st.session_state.level = level
+
 
     term = col1.text_input(
         "input",
