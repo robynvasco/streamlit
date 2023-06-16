b8cde7a55babbb6239e5f1147568c1cafcf6397c
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-16 09:34:33 +0200

    Update solve_linear_equation.py

--- solve_linear_equation.py
+++ solve_linear_equation.py
@@ -101,7 +101,7 @@ def main():
     )
    
 
-    if col2.button("Apply term", key="apply", on_click=lambda: enter(level)):
+    if col2.button("Apply term", key="apply", on_click=lambda: enter(level))
 
     if len(st.session_state['equations']) > 1:
         if col3.button("Undo", key="undo", on_click=clear_text):
