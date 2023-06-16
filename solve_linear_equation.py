743597869c85fffa880f06fe9031a29f2c29ac14
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-16 10:08:21 +0200

    Update solve_linear_equation.py

--- solve_linear_equation.py
+++ solve_linear_equation.py
@@ -104,12 +104,12 @@ def main():
     
     if col2.button("Apply term", on_click=clear_text):
         apply = True
-        enter(level)
+        apply_term(term, level)
    
     if len(st.session_state['equations']) > 1:
         if col3.button("Undo", key="undo", on_click=clear_text):
             apply = True
-            apply_term(term, level)
+            undo_last_action()
             
     
             
