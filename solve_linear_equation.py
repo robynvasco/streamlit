a4870a39d65f95fe3ef8ade317aedd33f6e0004c
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-19 11:49:22 +0200

    up

--- solve_linear_equation.py
+++ solve_linear_equation.py
@@ -129,7 +129,6 @@ def start_new_game(level):
             Eq((Symbol('x') - 5) / 2, 3+Symbol('x')),
             Eq((Symbol('x') * 3)+Symbol('x') / 4, 9),
             Eq((Symbol('x') + 2) + Symbol('x') / 3, 5),
-            Eq((Symbol('x') - 1) * 2, 8),
         ]
 
         # Add more levels and equation databases
