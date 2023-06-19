394c437d60d952332653dcd772ab196001a6b2e2
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-19 12:31:22 +0200

    up

--- solve_linear_equation.py
+++ solve_linear_equation.py
@@ -147,18 +147,17 @@ def start_new_game(level):
     st.session_state['terms'] = []
 
 def replace_decimals_with_fractions(equation):
-    new_equation = equation
-    x = Symbol('x')
-
-    if equation.lhs.has(Float):
-        new_lhs = nsimplify(equation.lhs)
-        new_equation = Eq(new_lhs, equation.rhs)
-
-    if equation.rhs.has(Float):
-        new_rhs = nsimplify(equation.rhs)
-        new_equation = Eq(equation.lhs, new_rhs)
-
-    return new_equation
+    if isinstance(equation, Eq):
+        lhs = replace_decimals_with_fractions(equation.lhs)
+        rhs = replace_decimals_with_fractions(equation.rhs)
+        return Eq(lhs, rhs)
+    elif isinstance(equation, Mul):
+        args = [replace_decimals_with_fractions(arg) for arg in equation.args]
+        return Mul(*args)
+    elif isinstance(equation, Float):
+        return Rational(equation).limit_denominator()
+    else:
+        return equation
 
 
 
