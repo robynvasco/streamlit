434ad9a2d378fba1d474a6e22f24704250f4c188
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-19 11:56:38 +0200

    up

--- solve_linear_equation.py
+++ solve_linear_equation.py
@@ -18,7 +18,7 @@ def apply_term(new_term, level):
         st.session_state['terms'].append(term)
 
         # Check if x is isolated
-         if isinstance(equation.lhs, Symbol) and equation.lhs == Symbol('x') and not equation.rhs.free_symbols:
+        if isinstance(equation.lhs, Symbol) and equation.lhs == Symbol('x') and not equation.rhs.free_symbols:
             st.balloons()
             st.success("Congratulations! You have isolated x and found the solution!")
             
