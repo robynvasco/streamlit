61e32329ce5edcf27da7d72b0bf53f2e0002b55b
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-21 23:40:26 +0200

    Update rule_of_three.py

--- rule_of_three.py
+++ rule_of_three.py
@@ -31,69 +31,112 @@ def plot_triangle(a, b, c):
     ax.text(a-0.1, b/2, "b", ha='right', va='center', color=b_color)
     ax.text(c-0.5, -0.7, "c", ha='center', va='bottom', color=c_color)
     ax.text(c-0.1, x/2, "x", ha='right', va='center', color=x_color)
-    
+
     # Set plot limits
     ax.set_xlim(0, 11)
     ax.set_ylim(0, 16)
-   
-    
+
     # Hide axes and background
     ax.axis('off')
     ax.set_facecolor('none')
-    
+
     # Show legend
     ax.legend(fontsize='x-small', loc='upper left')
-    
+
     # Show plot
     st.pyplot(fig)
 
-def main():
+def rule_of_three():
     st.title('Rule of Three')
-    
+
     # Constants
     a = 2
     b = 3
-    f=2/3
+
     # Calculate x
     x = lambda c: (b * c) / a
-    
+
     st.info("If two centimeters on a map are equivalent to 3 km, then how far is the real distance between two points on the map that are 5 cm apart?")
     # User input for c
     c = st.slider('Map Distance c in cm', min_value=1, max_value=10, value=4)
-    
-    
+
     # Plot triangle
     plot_triangle(a, b, c)
+
     # Calculate distance
     distance = x(5)
-    
+
     # Display formula in st.info
- 
     st.info(f"a is to b as c is to x.\n\n"
             f"$\\frac{{a}}{{b}} = \\frac{{c}}{{x}}$\n")
-    
+
     with st.expander("Explanation"):
-                st.write("The rule of three, also known as the proportionality rule, is a mathematical concept that allows us to solve proportional relationships between different quantities. It is based on the idea that if two ratios are equal, then the corresponding values in those ratios are also equal.")
-                st.write("In the sentence 'a is to b as c is to x,' implies that the ratio of a to b is equal to the ratio of c to x. So in our example, if the distance of two points on the map gets bigger, the real distance gets bigger proportionally.")
-                st.write("To use this sentence, we can interpret it as a mathematical equation:")
-                st.latex(r"\frac{a}{b} = \frac{c}{x}")
-                st.write("It implies that if we know three of the variables, we can calculate the value of the fourth variable.")
-        
-    
+        st.write("The rule of three, also known as the proportionality rule, is a mathematical concept that allows us to solve proportional relationships between different quantities. It is based on the idea that if two ratios are equal, then the corresponding values in those ratios are also equal.")
+        st.write("In the sentence 'a is to b as c is to x,' it implies that the ratio of a to b is equal to the ratio of c to x. So in our example, if the distance between two points on the map increases, the real distance also increases proportionally.")
+        st.write("To use this sentence, we can interpret it as a mathematical equation:")
+        st.latex(r"\frac{a}{b} = \frac{c}{x}")
+        st.write("This equation states that the ratio of a to b is equal to the ratio of c to x. It implies that if we know three of the variables, we can calculate the value of the fourth variable.")
+
     st.write("")
-    answer = st.text_input("How far is the real distance between two points on the map that are 5 cm apart? Enter your answer for x in km:", placeholder="type here your answer e.g. 3.0 or 4.5")
+    answer = st.text_input("How far is the real distance between two points on the map that are 5 cm apart? Enter your answer for x in km:", placeholder="Type your answer here, e.g., 3.0 or 4.5")
     if answer:
         try:
             if float(answer) == distance:
-                st.success(f"That's correct! A distance of 5 cm on the map is equivalent to a real distance of x={distance} km")
+                st.success(f"That's correct! A distance of 5 cm on the map is equivalent to a real distance of x = {distance} km.")
             else:
                 st.error("That's incorrect. Try again!")
         except ValueError:
             st.error("Invalid input. Please enter a number.")
-           
 
+def inverse_rule_of_three():
+    st.title('Inverse Rule of Three')
+
+    # Constants
+    a = 2
+    c = 5
+
+    # Calculate b
+    b = lambda x: (a * x) / c
+
+    st.info("If two centimeters on a map are equivalent to 3 km, what is the distance on the map between two points that are 4 km apart in real life?")
+    # User input for x
+    x = st.slider('Real Distance x in km', min_value=1, max_value=10, value=3)
+
+    # Plot triangle
+    plot_triangle(a, b(x), c)
+
+    # Calculate distance
+    distance = b(4)
+
+    # Display formula in st.info
+    st.info(f"a is to b as c is to x.\n\n"
+            f"$\\frac{{a}}{{b}} = \\frac{{c}}{{x}}$\n")
+
+    with st.expander("Explanation"):
+        st.write("The inverse rule of three is used when we want to find a value on one side of the ratio given the value on the other side. In this case, we know the values of a and c, and we want to find the value of b. We can rearrange the equation:")
+        st.latex(r"b = \frac{{a \cdot x}}{{c}}")
+        st.write("This equation states that the ratio of a to b is equal to the ratio of c to x. By rearranging the equation, we can solve for b.")
+
+    st.write("")
+    answer = st.text_input("What is the distance on the map between two points that are 4 km apart in real life? Enter your answer for b in cm:", placeholder="Type your answer here, e.g., 6.0 or 9.5")
+    if answer:
+        try:
+            if float(answer) == distance:
+                st.success(f"That's correct! A real distance of 4 km corresponds to a map distance of b = {distance} cm.")
+            else:
+                st.error("That's incorrect. Try again!")
+        except ValueError:
+            st.error("Invalid input. Please enter a number.")
+
+def main():
+    st.title("Rule of Three and Inverse Rule of Three")
     
+    tab1, tab2 = st.tabs(["Rule of Three", "Inverse Rule of Three"])
     
+    if tab1:
+        rule_of_three()
+    elif tab2:
+        inverse_rule_of_three()
 
 if __name__ == '__main__':
     main()
