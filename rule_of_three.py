bea943a8155e32865bad6184a548a1e45a1ee23b
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-21 13:56:56 +0200

    Update rule_of_three.py

--- rule_of_three.py
+++ rule_of_three.py
@@ -17,10 +17,10 @@ def plot_triangle(a, b, c):
   
     
     # Add labels
-    ax.text(a/2, 0, "a", ha='center', va='bottom', color='#006F5F')
-    ax.text(a-0.1, b/2, "b", ha='right', va='center', color='#00BFFF')
-    ax.text(c-0.5, 0, "c", ha='center', va='bottom', color='#00B894')
-    ax.text(c-0.1, x/2, "x", ha='right', va='center', color='#7FDBFF')
+    ax.text(a/2, 0, "a", ha='center', va='bottom', color='#00BFFF')
+    ax.text(a-0.1, b/2, "b", ha='right', va='center', color='#006F5F')
+    ax.text(c-0.5, 0, "c", ha='center', va='bottom', color='#7FDBFF')
+    ax.text(c-0.1, x/2, "x", ha='right', va='center', color='#00B894')
     
     # Set plot limits
     ax.set_xlim(0, 11)
