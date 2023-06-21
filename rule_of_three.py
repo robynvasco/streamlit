7d001879b339c49756dde50f38203ffda6dc2f41
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-21 14:40:00 +0200

    Update rule_of_three.py

--- rule_of_three.py
+++ rule_of_three.py
@@ -58,10 +58,12 @@ def main():
     distance = x(c)
     
     # Display formula in st.info
+ 
     st.info(f"a is to b as c is to x.\n\n"
          f"$\\frac{{a}}{{b}} = \\frac{{c}}{{x}}$\n\n"
-         f"\n$x = \\frac{{3km}}{{2cm}} \\cdot {c}cm = {distance} \text{km}$\n\n"   
+         f"\n$x = \\frac{{3\\,\\mathrm{{km}}}}{{2\\,\\mathrm{{cm}}}} \\cdot {c}\\,\\mathrm{{cm}} = {distance} \\mathrm{{km}}$\n\n "
          f"A distance of {c} cm on the map is equivalent to a real distance of x={distance} km.")
+
    
 
      # Print distance
