1e61a05f19aae03c0b9d7cb655e5fb666cb7facc
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-06-21 13:44:48 +0200

    Update rule_of_three.py

--- rule_of_three.py
+++ rule_of_three.py
@@ -23,9 +23,7 @@ def plot_triangle(a, b, c):
     # Set plot limits
     ax.set_xlim(0, max(a, c) + 1)
     ax.set_ylim(0, max(b, x) + 1)
-    
-    # Set aspect ratio
-    ax.set_aspect('equal')
+   
     
     # Hide axes and background
     ax.axis('off')
